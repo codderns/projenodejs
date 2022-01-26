291447bea3a288bb06d5a15897d020f3647e0069
nsgnd <53365066+nsgnd@users.noreply.github.com>
2022-01-26 10:41:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const connectionstring = "mongodb+srv://user_techcareer:evEejipk5gtTnHk2@cluster0.wmhvu.mongodb.net/techcareermusicdb?authSource=admin&replicaSet=atlas-uiaw03-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+const connectionstring = "......"
 
 module.exports = {
     connectionstring
